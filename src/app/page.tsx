7deddac4ff5a60b4850941c2dc09d1aac239f8b3
Daniel Leclaire <leclaired@outlook.com>
2024-10-16 00:16:02 -0500

    Search bar now lets you search via id of card
    Still need to work out limiting amount shown. and actually searching by name.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,47 @@
+"use client";
 import './App.scss';
 import Image from 'next/image';
 import React from 'react';
 import rawData from './cardinfo.php.json';
+import { useState } from "react";
 
-
-export default function home() {
-  const imgs: string[] = [];
-
+export default function MyComponent() {
+  const [query, setQuery] = useState('');
   const parsedData = JSON.parse(JSON.stringify(rawData));
-	for(let i = 0; i < 400; i++){
+  const imgs: string[] = [];
+  for(let i = 0; i < parsedData.data.length; i++){
     const currCard = parsedData.data[i].id
-		imgs[i] = ("/cardImages/"+currCard+".jpg");
-	}
-	const cards = imgs.map(function(image) {
- 		return (<Image alt="" key={image} src={(image)} className="card" />);
-	});
+    imgs[i] = ("/cardImages/"+currCard+".jpg");
+  }
+  //search filter
+  const searchFilter = (array: string[]) => {
+    console.log(array)
+    return array.filter(
+      (el) => el.toLowerCase().includes(query)
+    )
+  }
+
+  const filtered = searchFilter(imgs);
+  console.log(filtered)
+
+  const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setQuery(e.target.value)
+  }
+
+  const cards = filtered.map(function(image) {
+    return (<Image alt="" key={image} src={(image)} className="card" />);
+ });
+
   return (
     <div className="page">
+      <input onChange={handleChange} type='text' placeholder='dark' color='black' />
     	<div className="cardContainer">
         {cards}
         </div>
     </div>
   );
 }
+
+
+
+
